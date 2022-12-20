3ca1ebc9abb408db3d6b35f9a2ce137de42f31a6
Moonbed94 <120119459+Moonbed94@users.noreply.github.com>
2022-12-20 19:38:06 +0000

    Added Movement and Jumping
    Able to move in all directions and jump. Cam follows player and whichever way you face and push W you'll go forwards in the direction. As well as backwards and side to side :)

--- Untitled Zombie Game V0.1/Assets/Scrpits/PlayerMovement.cs
+++ Untitled Zombie Game V0.1/Assets/Scrpits/PlayerMovement.cs
@@ -6,11 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed; 
+    public float rotationSpeed;
+    public float jumpSpeed;
+
+    public Transform cam; 
+
+    private bool isGrounded; 
+
+    private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset; 
+    private float turnSmoothVelocity;
+    private float turnSmoothTime = 0.1f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset; 
     }
 
     // Update is called once per frame
@@ -19,9 +33,51 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal"); 
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = direction.magnitude;
+        magnitude = Mathf.Clamp01(magnitude);
+        direction.Normalize();
 
-        transform.Translate(movementDirection * speed * Time.deltaTime);
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed; 
+            }
+    
+        }
+
+        else
+        {
+            characterController.stepOffset = 0f;
+        }
+
+
+        Vector3 velocity = direction * magnitude * speed;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+
+        /*if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }*/
     } 
 }
